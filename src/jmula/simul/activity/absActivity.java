42de16eb821c6c26754e83753381b6ff16ea8591
Natanael Ramos <naelr8@gmail.com>
2015-06-29 22:06:05 -0300

    Classe para tratamento de atividades do modelo.

---
+++ src/jmula/simul/activity/absActivity.java
@@ -0,0 +1,14 @@
+package jmula.simul.activity;
+
+/**
+ * Created by nael on 29/06/15.
+ */
+public abstract class absActivity
+{
+	/**
+	 * Condições de início da atividade.
+	 *
+	 * @return true se a atividade pode iniciar
+	 */
+	public abstract boolean condition();
+}
